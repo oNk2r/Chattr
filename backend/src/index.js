a99a4dc93b82155f50f4b506bfc8c150c7e39adf
oNk2r <onkartulshigar@gmail.com>
2026-06-20 20:47:37 +0530

    feat: Add dockerfile and dockerignore

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,8 @@ import connectDB from "./lib/db.js";
 import cors from "cors";
 import dns from "dns";
 import { clerkMiddleware } from "@clerk/express";
+import fs from "fs";
+import path from "path";
 
 dotenv.config();
 
@@ -16,6 +18,8 @@ const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
+const publicDir = path.join(process.cwd(), "public");
+
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware());
@@ -23,6 +27,14 @@ app.use(clerkMiddleware());
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
+
+if(fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) =>next(err));
+  });
+}
+
 
 app.listen(PORT, () => {
   connectDB();
