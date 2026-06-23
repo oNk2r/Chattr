d8997dc0372b491bdb7f31f8038d784419da1e97
oNk2r <onkartulshigar@gmail.com>
2026-06-23 22:03:41 +0530

    feat: initialize frontend project with Clerk auth, theme/wallpaper contexts, and Tailwind CSS integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,30 @@
 import { useState } from 'react'
-import './App.css'
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { Routes, Route, Navigate } from 'react-router'
+import { ThemeProvider } from './context/ThemeContext'
+import { WallpaperProvider } from './context/WallpaperContext'
+import ChatPage from './pages/ChatPage'
+import AuthPage from './pages/AuthPage'
+import { useAuth } from '@clerk/react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if(!isLoaded) return <p>loading...</p>
 
   return (
-    <div>
-      <h1>Chattr</h1>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route
+            path="/auth"
+            element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+          />
+        </Routes>
+        <Toaster />
+      </WallpaperProvider>
+    </ThemeProvider>
   )
 }
 
